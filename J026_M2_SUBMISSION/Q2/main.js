cab27587534b2597f7342d16c154450780560f66
SameerMulani <88852494+SameerMulani@users.noreply.github.com>
2022-09-25 14:49:39 +0530

    Add files via upload

---
+++ J026_M2_SUBMISSION/Q2/main.js
@@ -0,0 +1,8 @@
+// Select total count
+// const totalCount = document.getElementById("demo");
+
+// // Variable to track count
+// var count = 0;
+
+// // Display initial count value
+// totalCount.innerHTML = count;
